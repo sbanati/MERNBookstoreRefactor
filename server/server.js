efee158a790a9a64c44523fae1e7cac7e982dd09
smbanati-IZxCKm <smbanati@gmail.com>
2024-04-19 16:41:24 -0400

    setup apollo server for serverjs

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+// import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
 
+// import middleware function for authenticating
+const { authMiddleware } = require('./utils/auth');
+
+//import typeDefs and resolvers
+const {typeDefs, resolvers } = require('./schemas')
+
+const db = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass schema data 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +31,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+
+// wildcard get route to any location on the server that doesn't have an explicit route defined
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
