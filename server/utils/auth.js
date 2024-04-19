64a003f12a48861b9b8d2d8beeffbe2c83527ada
smbanati-IZxCKm <smbanati@gmail.com>
2024-04-19 16:12:16 -0400

    updated auth middleware for graphql api

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,4 @@
+
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -6,17 +7,17 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function (req) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
-    // ["Bearer", "<tokenvalue>"]
+    // separate bearer from token value ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -25,15 +26,14 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return the request object so it can be passed to the resolver as `context`
+    return req;
+    
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
