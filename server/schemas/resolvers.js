e730e12b2f3ec155bfae417ff8aea1c674391f1c
smbanati-IZxCKm <smbanati@gmail.com>
2024-04-20 01:54:02 -0400

    resolvers query and login mutation

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,64 @@
+// Define the query and mutations to work with Mongoose Models 
+const { User} = require('../models');
+const { AuthenticationError} = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('book');
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+        users: async () => {
+            return User.find().select("-__v -password").populate("book");
+        },
+        user: async (parent, {username}) => {
+            return User.findOne({username})
+                .select("-__v -password")
+                .populate("book");
+        },
+    },
+    
+    Mutation: {
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError("Incorrect credentials")
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError("Incorrect credentials")
+            }
+            const token = signToken(user);
+            return { token, user };
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    },
+}
+module.exports = resolvers;
